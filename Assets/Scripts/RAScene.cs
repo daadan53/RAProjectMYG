a74d378d460c73e4f39bdaf423c78ac28947a9bf
daadan53 <dadan53unity@gmail.com>
2024-11-14 17:57:33 +0100

    rotateVisual

--- Assets/Scripts/RAScene.cs
+++ Assets/Scripts/RAScene.cs
@@ -4,6 +4,8 @@ public class RAScene : MonoBehaviour
 {
     [SerializeField] private GameObject[] prefabs;
     private GameObject visualRA;
+    private const string wineBottle = "WineBottle";
+    [SerializeField] private int rotationDegree = 10;
 
     private void Awake() 
     {
@@ -20,8 +22,27 @@ public class RAScene : MonoBehaviour
 
     public void GoBack() => GameManager.instance.OnGoBack("CatalogueScene");
     
-    /*public void OnTargetFound()
-    { 
-        this.gameObject.transform.GetChild(0).gameObject.SetActive(true);
-    }*/
+    public void TurnRight()
+    {
+        if(visualRA.name != wineBottle)
+        {
+            visualRA.transform.Rotate(0, visualRA.transform.rotation.y - rotationDegree, 0);
+        }
+        else
+        {
+            visualRA.transform.Rotate(0, 0, visualRA.transform.rotation.z - rotationDegree);
+        }
+    }
+
+    public void TurnLeft()
+    {
+        if(visualRA.name != wineBottle)
+        {
+            visualRA.transform.Rotate(0, visualRA.transform.rotation.y + rotationDegree, 0);
+        }
+        else
+        {
+            visualRA.transform.Rotate(0, 0, visualRA.transform.rotation.z + rotationDegree);
+        }
+    }
 }
